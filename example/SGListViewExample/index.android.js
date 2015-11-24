dbd63ace68c2143dbc79783482c5f9551499d11b
Ricky Reusser <rsreusser@gmail.com>
2015-11-24 11:28:49 -0500

    Add example

---
+++ example/SGListViewExample/index.android.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var React = require('react-native')
+
+import Example from './example'
+
+var {
+  AppRegistry,
+} = React;
+
+AppRegistry.registerComponent('SGListViewExample', () => Example);
