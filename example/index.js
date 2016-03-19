e4c433494a847030c678528ada4da6135a20101d
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2016-03-19 16:53:31 -0700

    Added basic listview

--- example/index.js
+++ example/index.js
@@ -6,26 +6,30 @@
 import React, {
   AppRegistry,
   Component,
+  ListView,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
 class example extends Component {
+
+  constructor(props) {
+    super(props);
+
+    // Instantiate the ListView's datasource
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Text>{rowData}</Text>}
+      />
     );
   }
 }
