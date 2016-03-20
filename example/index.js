923cb458ff046514298e4380eb945f953434e3c2
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2016-03-19 17:12:28 -0700

    Moar

--- example/index.js
+++ example/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import React, { AppRegistry, Component, ListView, StyleSheet, Text, View } from 'react-native';
+import ClientId from './config/clientId';
 
 class example extends Component {
 
@@ -9,9 +10,15 @@ class example extends Component {
 
     // Instantiate the ListView's datasource
     var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
     this.state = {
       dataSource: ds.cloneWithRows(['row 1', 'row 2']),
     };
+
+    this.cache = {
+      offset: 0,
+      limit: 10,
+    }
   }
 
   render() {
@@ -21,6 +28,14 @@ class example extends Component {
         renderRow={(rowData) => <Text>{rowData}</Text>}
       />
     );
+  }
+
+  generateQuery(callback) {
+    let currentLocation = DeviceLocationManager.currentLocation();
+    let rapiQuery = "https://api.groupon.com/cards/v1/search?shaheen=awwyea&offset=" + this.cache.offset + "&client_id=" + ClientId.clientId + "&client_version_id=" + ClientId.clientVersion + "&ell=" + lat + "%2C" + lng + "&limit=" + limit + "&locale=en_US&page_type=featured&platform=iphonecon&secure_assets=false&show=deals%28default%2CstartRedemptionAt%2CendRedemptionAt%2Coptions%28images%2Ctrait%2CpricingMetadata%29%2Clocations%2Ctraits%2Cgrid4ImageUrl%2ClargeImageUrl%2CsidebarImageUrl%2Cchannels%2Cimages%2Cmerchant%28recommendations%29%29%3B_badges&sort=relevance&visitor_id=E93BC28B-85E0-4266-D2BC-7E1A17384290";
+
+
+    callback(false, query);
   }
 }
 
