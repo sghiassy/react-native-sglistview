9523c98f9a47a24237a87d1af2781fd37631aca1
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2016-03-19 17:22:15 -0700

    Making network request

--- example/index.js
+++ example/index.js
@@ -2,6 +2,7 @@
 
 import React, { AppRegistry, Component, ListView, StyleSheet, Text, View } from 'react-native';
 import ClientId from './config/clientId';
+import http from 'superagent';
 
 class example extends Component {
 
@@ -12,30 +13,51 @@ class example extends Component {
     var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
     this.state = {
-      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+      deals: undefined,
     };
 
     this.cache = {
       offset: 0,
       limit: 10,
-    }
+      lat: 45.52,
+      lng: -122.681944
+    };
+
+    this.getDeals();
+  }
+
+  getDataSource() {
+    var dataSource = new ListView.DataSource(
+      {rowHasChanged: (r1, r2) => r1.uuid !== r2.uuid});
+
+    var deals = this.props.deals;
+    return deals ? dataSource.cloneWithRows(this.state.deals) : dataSource;
   }
 
   render() {
     return (
       <ListView
-        dataSource={this.state.dataSource}
+        dataSource={this.getDataSource()}
         renderRow={(rowData) => <Text>{rowData}</Text>}
       />
     );
   }
 
-  generateQuery(callback) {
-    let currentLocation = DeviceLocationManager.currentLocation();
-    let rapiQuery = "https://api.groupon.com/cards/v1/search?shaheen=awwyea&offset=" + this.cache.offset + "&client_id=" + ClientId.clientId + "&client_version_id=" + ClientId.clientVersion + "&ell=" + lat + "%2C" + lng + "&limit=" + limit + "&locale=en_US&page_type=featured&platform=iphonecon&secure_assets=false&show=deals%28default%2CstartRedemptionAt%2CendRedemptionAt%2Coptions%28images%2Ctrait%2CpricingMetadata%29%2Clocations%2Ctraits%2Cgrid4ImageUrl%2ClargeImageUrl%2CsidebarImageUrl%2Cchannels%2Cimages%2Cmerchant%28recommendations%29%29%3B_badges&sort=relevance&visitor_id=E93BC28B-85E0-4266-D2BC-7E1A17384290";
+  getDeals() {
+    var self = this;
+    var query = "https://api.groupon.com/cards/v1/search?offset=" + this.cache.offset
+                      + "&client_id=" + ClientId.clientId
+                      + "&client_version_id=" + ClientId.clientVersion
+                      + "&ell=" + this.cache.lat + "%2C" + this.cache.lng
+                      + "&limit=" + this.cache.limit + "&locale=en_US&page_type=featured&platform=iphonecon&secure_assets=false&show=deals%28default%2CstartRedemptionAt%2CendRedemptionAt%2Coptions%28images%2Ctrait%2CpricingMetadata%29%2Clocations%2Ctraits%2Cgrid4ImageUrl%2ClargeImageUrl%2CsidebarImageUrl%2Cchannels%2Cimages%2Cmerchant%28recommendations%29%29%3B_badges&sort=relevance";
 
 
-    callback(false, query);
+    http.get(query)
+     .end((err, res) => {
+       if (err) { return callback(err, undefined) };
+
+       self.setState({deals:res.body});
+     });
   }
 }
 
