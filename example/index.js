a61918e065be6e3a8fe676b0620bf836cc276486
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2016-03-19 18:25:34 -0700

    ISSUE-14: Updated SGListView to respond directly to getScrollResponder function call

---
+++ example/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+import React, { AppRegistry, StyleSheet } from 'react-native';
+import CardList from './src/CardListController';
+
+class example extends React.Component {
+  render() {
+    return (
+      <CardList style={styles.container}/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+AppRegistry.registerComponent('example', () => example);
