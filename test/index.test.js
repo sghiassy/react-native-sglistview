c8ccf1b5a2d32c1636379ff6e1dd847c0d6f1b0a
Shaheen Ghiassy <s.ghiassy@gmail.com>
2016-06-15 16:34:08 -0700

    its rendering

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,22 @@
 const should = require('should');
+const { shallow } = require('enzyme');
 const SGListView = require('../dist');
+const React = require('React');
 
 describe('SGListView', () => {
+
+  let sgListView;
+
+  before(() => {
+    sgListView = shallow(
+      <SGListView />
+    );
+  });
+
   it('should exist', () => {
     SGListView.should.be.ok;
+    sgListView.should.be.ok;
+    console.log(sgListView);
   });
+
 });
