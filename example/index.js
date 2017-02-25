295a67946842bdbba3f7a92985edd772a4c1b071
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2017-02-25 09:07:10 -0800

    Example app working again

---
+++ example/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import React from 'React';
+import { AppRegistry, StyleSheet } from 'react-native';
+import CardList from './src/CardListController';
+
+class example extends React.Component {
+  render() {
+    return (
+      <CardList style={styles.container}/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => example);
